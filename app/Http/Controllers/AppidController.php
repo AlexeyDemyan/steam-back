8c799a7ae25c71e003e6563fdcb3e857b30b8330
Alexey Demyanov <deal110988@him-mail.ch>
2025-03-20 16:22:44 +0100

    retrieving appids via GET

--- app/Http/Controllers/AppidController.php
+++ app/Http/Controllers/AppidController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Appid;
 
 class AppidController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $appids = DB::select("SELECT * FROM appids");
+        return response()->json($appids);
+    }
 }
